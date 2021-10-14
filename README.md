aa66f084c3b9a9d74b6f4feb077c6ee6a19b58bc
Jackson Visser <jvisser@pugetsound.edu>
2021-10-13 23:17:13 -0700

    final commit before deadline. cleaned up code and added comments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-CS 240 HW #3
+CS 240 HW #3: .CSS PARK
+
+**NOTE: I forgot what you said in class Tuesday about following the directions exactly (with font size, etc.) and instead
+modeled my scoreboard and control center to model what your working solution looked like as close as possible.**
+
+Make Some Noise image source: https://imgr.search.brave.com/Jf81P1twZ7RnHoPjSKOgx-b9u9HT0eJNXKS2mZ1n0bM/fit/1200/1200/ce/1/aHR0cHM6Ly9maWxl/cy53aG9vc2hrYWEu/Y29tL3BvZGNhc3Rz/L3BvZGNhc3RfNTU3/OS9wb2RjYXN0X21l/ZGlhLzk5YzI1Ny1N/YWtlX1NvbWVfTm9p/c2UuanBn
+
+I made the Kiss Cam and Rhino Charge images myself in Google Slides.
--- app.js
+++ app.js
@@ -5,6 +5,28 @@
 //query for box nodes
 let boxes = document.querySelectorAll(".inning-scores");
 
+//get away and home total runs nodes
+let awayRunTotalNode = document.querySelector("#awayR");
+let homeRunTotalNode = document.querySelector("#homeR");
+
+//query for manipulable elements from control center
+let whoNode = document.querySelector("#who-toggle");
+let inningNode = document.querySelector("#inning-options");
+let runsPlusNode = document.querySelector("#runs-plus");
+let runsMinusNode = document.querySelector("#runs-minus");
+let musicNoteNode = document.querySelector("#music-note");
+let keyboardNode = document.querySelector("#keyboard");
+
+//get image node
+let imageNode = document.querySelector("img");
+console.dir(imageNode);
+
+//add audio files
+chickenDanceSong = new Audio("sounds/chickendance.mp3");
+buildupSong = new Audio("sounds/buildup.mp3");
+charge1Song = new Audio("sounds/charge1.mp3");
+charge2Song = new Audio("sounds/charge2.mp3");
+
 //store box nodes themselves in two arrays (one for home, one for away)
 let awayBoxes = [];
 let homeBoxes = [];
@@ -17,33 +39,18 @@ for (let i = 7; i < boxes.length; i++) {
   homeBoxes.push(boxes[i]);
 }
 
-//query for manipulable elements from control center
-let whoNode = document.querySelector("#who-toggle");
-let inningNode = document.querySelector("#inning-options");
-let runsPlusNode = document.querySelector("#runs-plus");
-let runsMinusNode = document.querySelector("#runs-minus");
-let musicNoteNode = document.querySelector("#music-note");
-let keyboardNode = document.querySelector("#keyboard");
-
-//get away and home total runs nodes
-let awayRunTotalNode = document.querySelector("#awayR");
-let homeRunTotalNode = document.querySelector("#homeR");
-
-//get image node
-let imageNode = document.querySelector("img");
-console.dir(imageNode);
-
 //helper function to increment runs, used with event listener on + button
 function incrementRun() {
   let team = whoNode.value;
   let inning = inningNode.value;
-  let boxName = team + inning;
+  let boxName = team + inning; //this creates the correct id for each box
 
   if (team == "away") {
-    let boxNode = awayBoxes.filter((node) => node.id == boxName);
-    boxNode[0].innerHTML++;
-    awayRunTotalNode.innerHTML = sumRuns(awayBoxes);
+    let boxNode = awayBoxes.filter((node) => node.id == boxName); //gets the correct node from the list
+    boxNode[0].innerHTML++; //increment run
+    awayRunTotalNode.innerHTML = sumRuns(awayBoxes); //update sum
   } else {
+    //team is home. do the same thing
     let boxNode = homeBoxes.filter((node) => node.id == boxName);
     boxNode[0].innerHTML++;
     homeRunTotalNode.innerHTML = sumRuns(homeBoxes);
@@ -54,11 +61,12 @@ function incrementRun() {
 function decrementRun() {
   let team = whoNode.value;
   let inning = inningNode.value;
-  let boxName = team + inning;
+  let boxName = team + inning; //this gets the correct id for each box
 
   if (team == "away") {
-    let boxNode = awayBoxes.filter((node) => node.id == boxName);
+    let boxNode = awayBoxes.filter((node) => node.id == boxName); //gets the correct node from the list
     if (boxNode[0].innerHTML - 1 >= 0) {
+      //ensures runs cannot go negative, then do the same thing as incrementRun()
       boxNode[0].innerHTML--;
       awayRunTotalNode.innerHTML = sumRuns(awayBoxes);
     }
@@ -84,12 +92,6 @@ function sumRuns(boxes) {
 runsPlusNode.addEventListener("click", incrementRun);
 runsMinusNode.addEventListener("click", decrementRun);
 
-//add audio stuff
-chickenDanceSong = new Audio("sounds/chickendance.mp3");
-buildupSong = new Audio("sounds/buildup.mp3");
-charge1Song = new Audio("sounds/charge1.mp3");
-charge2Song = new Audio("sounds/charge2.mp3");
-
 //make it so pressing keyboard button plays chicken dance song
 keyboardNode.addEventListener("click", doChickenDance);
 
@@ -98,6 +100,7 @@ function doChickenDance() {
   chickenDanceSong.play();
   imageNode.src = "images/sdchicken.jpg";
   setTimeout(() => {
+    //changes image back to UPS logo after 10s
     imageNode.src = "images/ups.png";
   }, 10000);
 }
@@ -140,6 +143,7 @@ function doRandomMusicAndImage() {
       break;
   }
   setTimeout(() => {
+    //changes image back to UPS logo after 5s
     imageNode.src = "images/ups.png";
-  }, 10000);
+  }, 5000);
 }
